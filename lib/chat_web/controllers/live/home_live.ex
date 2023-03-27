1bf99bebaa7a63f4debff4a4470c7666ebdeffac
Zack Williams <zackwilldev@gmail.com>
2023-03-27 12:44:13 -0400

    building home page ran into core component bug

---
+++ lib/chat_web/controllers/live/home_live.ex
@@ -0,0 +1,7 @@
+defmodule ChatWeb.HomeLive do
+  use ChatWeb, :live_view
+
+  def mount(_params, _session, socket) do
+    {:ok, socket}
+  end
+end
--- lib/chat_web/router.ex
+++ lib/chat_web/router.ex
@@ -17,7 +17,7 @@ defmodule ChatWeb.Router do
   scope "/", ChatWeb do
     pipe_through :browser
 
-    live "/", PageController, :home
+    live "/", HomeLive, :home
   end
 
   # Other scopes may use custom stacks.
