d99e2f8154327f6ac57103ef8ae586d6a831102c
Zack Williams <zackwilldev@gmail.com>
2023-03-27 14:57:59 -0400

    update/fix core compoents, wrap up home page with new LiveView .18 syntax for topic

--- lib/chat_web/controllers/live/home_live.ex
+++ lib/chat_web/controllers/live/home_live.ex
@@ -2,6 +2,11 @@ defmodule ChatWeb.HomeLive do
   use ChatWeb, :live_view
 
   def mount(_params, _session, socket) do
-    {:ok, socket}
+    {:ok, assign(socket, form: to_form(%{}))}
+  end
+
+  def handle_event("goto_topic", %{"topic_name" => topic_name}, socket) do
+    topic_link = "/" <> topic_name
+    {:noreply, push_redirect(socket, to: topic_link)}
   end
 end
